c1f0db31c3dbe0a1030c1a268772fdf52fe1bbd7
Nguyen Minh Khoa <nmkhoa89@gmail.com>
2022-02-13 09:30:29 +0700

    update

--- contracts/box.sol
+++ contracts/box.sol
@@ -9,46 +9,60 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  contract Box is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    mapping (address => uint[] ) boxOwner;
-    event mintBox(uint _sum123,uint _timeBegin, uint _timeEnd, uint _price,string _sellingUint, uint _limitBox );
-    
+    struct EventInfo {
+        uint256 totalSupply;
+        uint256 boxPrice;
+        string currency;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 maxBuy;
+    }
+    Box[] private box;
+    mapping(uint256 => EventInfo) public eventByID;
+    event EventCreated(uint _totalSupply, uint _price, string _currency,uint _startTime,uint _endTime,uint _maxBuy);
+    event mintBox(uint _sum,address _recipient);
     constructor() ERC721("Box", "BOX") {}
-    uint private _timeBegin; 
-    uint private _timeEnd;
-    uint private _price;
-    string private _sellingUint;
-    uint private _limitBox;
-    function _mintNFT(address recipient, uint amountToMint )
-        private 
-        
-    {
-        for(uint i = 0; i < amountToMint; i++){
+   
+    function _mintNFT(address recipient)
+        private  
+    {   
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
-        boxOwner[recipient].push(newItemId);
         _mint(recipient, newItemId);
-    
-    }
         
     }
 
-    function setUp ( uint timeEnd, uint price,string memory sellingUint,uint limitBox) external onlyOwner {
-        _timeBegin = block.timestamp;
-        _timeEnd = timeEnd * 86400 + block.timestamp;
-        _price = price;
-        _sellingUint = sellingUint;
-        _limitBox = limitBox;
-        
-    }
-    function saleBox(uint sum,address recipient) external onlyOwner {
-        require(sum <= _limitBox);
-        require(_timeBegin <= block.timestamp &&  block.timestamp <= _timeEnd);
-        _mintNFT(recipient,sum );
-        if(sum == _limitBox) {
-            emit mintBox(sum,_timeBegin,_timeEnd, _price,_sellingUint,_limitBox);
-        }
+    function createEvent(
+        uint256 _eventID,
+        uint256 _totalSupply,
+        uint256 _price,
+        string memory _currency,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _maxBuy
+    ) external onlyOwner {
+        require(_totalSupply > 0, "Invalid Supply");
+        require(_startTime < _endTime, "Invalid time");
+        require(_maxBuy > 0, "Need set max buy");
 
+        eventByID[_eventID] = EventInfo(_totalSupply, _price, _currency, _startTime, _endTime, _maxBuy);
+        emit EventCreated(
+            _totalSupply,
+            _price,
+            _currency,
+            _startTime,
+            _endTime,
+            _maxBuy
+        );
+    }
+    function saleBox(uint eventID,uint sum) external payable {
+        require(msg.value == eventByID[eventID].boxPrice * sum );
+        require(sum < eventByID[eventID].maxBuy );
+        require(eventByID[eventID].startTime <= block.timestamp &&  block.timestamp <= eventByID[eventID].endTime);
+         for(uint8 i=0;i<sum;i++){
+             _mintNFT(msg.sender);
+         }
+        emit mintBox(sum,msg.sender);
     }
 
 }
